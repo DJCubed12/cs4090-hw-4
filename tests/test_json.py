c8fbb1d4c43da9af6e73feb0e5d0494c988156de
DJCubed12 <djcubed12@gmail.com>
2025-04-23 13:18:10 -0500

    Fix test_json.py to find its files better

--- tests/test_json.py
+++ tests/test_json.py
@@ -5,9 +5,13 @@ import os
 from ..src.tasks import load_tasks, save_tasks
 
 
+test_dir = os.path.dirname(__file__)
+
+
 def test_load_tasks_success():
     """Expects to be run in the root of the project (one level above tests folder)."""
-    tasks = load_tasks("tests/sample_data/tasks.json")
+    json_file = os.path.join(test_dir, "sample_data", "tasks.json")
+    tasks = load_tasks(json_file)
     assert len(tasks) == 3
 
 
@@ -18,22 +22,26 @@ def test_load_tasks_file_not_found():
 
 def test_load_tasks_invalid_json():
     """Expects to be run in the root of the project (one level above tests folder)."""
-    tasks = load_tasks("tests/sample_data/bad_json.json")
+    bad_json_file = os.path.join(test_dir, "sample_data", "bad_json.json")
+    tasks = load_tasks(bad_json_file)
     assert tasks == []
 
 
 def test_load_and_save():
     """Expects to be run in the root of the project (one level above tests folder)."""
-    loaded_tasks = load_tasks("tests/sample_data/tasks.json")
-    save_tasks(loaded_tasks, "tests/save_tasks_test_output.json")
+    json_file_path = os.path.join(test_dir, "sample_data", "tasks.json")
+    loaded_tasks = load_tasks(json_file_path)
 
-    assert os.path.exists("tests/save_tasks_test_output.json")
+    save_file_path = os.path.join(test_dir, "save_tasks_test_output.json")
+    save_tasks(loaded_tasks, save_file_path)
 
-    with open("tests/sample_data/tasks.json", "r") as original_file:
+    assert os.path.exists(save_file_path)
+
+    with open(json_file_path, "r") as original_file:
         original_file_contents = original_file.read()
-    with open("tests/save_tasks_test_output.json", "r") as saved_file:
+    with open(save_file_path, "r") as saved_file:
         saved_file_contents = saved_file.read()
 
     assert original_file_contents == saved_file_contents
 
-    os.remove("tests/save_tasks_test_output.json")
+    os.remove(save_file_path)
