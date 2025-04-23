553ba4bbf2d2be848c616f1bd7e0bf9d8fe808a3
DJCubed12 <djcubed12@gmail.com>
2025-04-23 13:22:58 -0500

    Combine basic tests into one file

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,6 @@
 import pytest
 
+import os
 import datetime
 
 from ..src.tasks import (
@@ -9,7 +10,11 @@ from ..src.tasks import (
     filter_tasks_by_priority,
     get_overdue_tasks,
     search_tasks,
+    load_tasks,
+    save_tasks,
 )
+
+TEST_DIR = os.path.dirname(__file__)
 
 
 def test_generate_unique_id_empty():
@@ -117,7 +122,7 @@ def test_get_overdue_tasks():
         {
             "id": 1,
             "completed": False,
-            "due_date": datetime.datetime(2020, 1, 1).strftime("%Y-%m-%d"),
+            "due_date": datetime.datetime(2025, 4, 10).strftime("%Y-%m-%d"),
         },
         # Old but completed
         {
@@ -148,3 +153,42 @@ def test_get_overdue_tasks():
     assert len(overdue) == 2
     assert tasks[0] in overdue
     assert tasks[1] in overdue
+
+
+def test_load_tasks_success():
+    """Expects to be run in the root of the project (one level above tests folder)."""
+    json_file = os.path.join(TEST_DIR, "sample_data", "tasks.json")
+    tasks = load_tasks(json_file)
+    assert len(tasks) == 3
+
+
+def test_load_tasks_file_not_found():
+    tasks = load_tasks("this_is_not_a_file.json")
+    assert tasks == []
+
+
+def test_load_tasks_invalid_json():
+    """Expects to be run in the root of the project (one level above tests folder)."""
+    bad_json_file = os.path.join(TEST_DIR, "sample_data", "bad_json.json")
+    tasks = load_tasks(bad_json_file)
+    assert tasks == []
+
+
+def test_load_and_save():
+    """Expects to be run in the root of the project (one level above tests folder)."""
+    json_file_path = os.path.join(TEST_DIR, "sample_data", "tasks.json")
+    loaded_tasks = load_tasks(json_file_path)
+
+    save_file_path = os.path.join(TEST_DIR, "save_tasks_test_output.json")
+    save_tasks(loaded_tasks, save_file_path)
+
+    assert os.path.exists(save_file_path)
+
+    with open(json_file_path, "r") as original_file:
+        original_file_contents = original_file.read()
+    with open(save_file_path, "r") as saved_file:
+        saved_file_contents = saved_file.read()
+
+    assert original_file_contents == saved_file_contents
+
+    os.remove(save_file_path)
